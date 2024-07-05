57be0811c0462014f271f83f7adf2936c15dd511
Alain <alain00.alvarez@gmail.com>
2024-07-05 10:34:02 -0400

    chore: add g analytics

--- src/components/face.tsx
+++ src/components/face.tsx
@@ -43,49 +43,53 @@ export const Face = ({}: FaceProps) => {
   }, [])
   
   return (
-    <svg
-      width="100"
-      height="100"
-      viewBox="0 0 12.7 12.7"
-      version="1.1"
-      className="relative z-50 fill-background stroke-background"
-      id="svg1"
-    >
-      <g
-        id="layer1">
-        
-        <motion.rect
-          style={{strokeWidth: 0.264583 }}
-          id="rect1"
-          width="1"
-          variants={eyesVariants}
-          animate={eyesState}
-          x="2.1577501"
-          // y="5.0231566" 
-          />
-        <motion.rect
-          style={{strokeWidth: 0.264583 }}
-          id="rect1-5"
-          width="1"
-          variants={eyesVariants}
-          animate={eyesState}
-          x="8.6706848"
-          // y="5.1072135" 
-          />
-        <path
-          style={{ fill: 'none', strokeWidth: 0.464583, strokeOpacity: 1 }}
-          d="
-            m 1.6570978,8.8396327 
-            c 
-              0,0 
-              0.9881717,1.5246783 
-              4.417846,1.5197643 
-              2.9569278,-0.0042 
-              3.9819272,-1.3984224 
-              3.9819272,-1.3984224
-          "
-          id="path1" />
-      </g>
-    </svg>
+    <div 
+      // className="w-[150px] h-[150px] flex justify-center items-center rounded-full bg-background/5 relative z-50"
+      >
+      <svg
+        width="100"
+        height="100"
+        viewBox="0 0 12.7 12.7"
+        version="1.1"
+        className="relative z-50 fill-background stroke-background"
+        id="svg1"
+      >
+        <g
+          id="layer1">
+          
+          <motion.rect
+            style={{strokeWidth: 0.264583 }}
+            id="rect1"
+            width="1"
+            variants={eyesVariants}
+            animate={eyesState}
+            x="2.1577501"
+            // y="5.0231566" 
+            />
+          <motion.rect
+            style={{strokeWidth: 0.264583 }}
+            id="rect1-5"
+            width="1"
+            variants={eyesVariants}
+            animate={eyesState}
+            x="8.6706848"
+            // y="5.1072135" 
+            />
+          <path
+            style={{ fill: 'none', strokeWidth: 0.464583, strokeOpacity: 1 }}
+            d="
+              m 1.6570978,8.8396327 
+              c 
+                0,0 
+                0.9881717,1.5246783 
+                4.417846,1.5197643 
+                2.9569278,-0.0042 
+                3.9819272,-1.3984224 
+                3.9819272,-1.3984224
+            "
+            id="path1" />
+        </g>
+      </svg>
+    </div>
   )
 }
